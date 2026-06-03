c49e79c446491d5b9ea2dd2e9c69d88fb8837bbe
mxilia <ravintkosol@gmail.com>
2026-06-03 18:40:11 +0700

    add user usecase

--- internal/usecase/contracts.go
+++ internal/usecase/contracts.go
@@ -1,25 +1,27 @@
 package usecase
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 	"github.com/mxilia/go-user-microservice.git/internal/entities"
 )
 
 type (
 	UserUseCase interface {
-		CreateUser(user *entities.User) (*entities.User, error)
-		FindAllUsers(page int, limit int) ([]*entities.User, int64, error)
-		FindUserById(id uuid.UUID) (*entities.User, error)
-		FindUserByHandler(handler string) (*entities.User, error)
-		FindUserByEmail(email string) (*entities.User, error)
-		PatchUser(id uuid.UUID, user *entities.User) error
-		DeleteUser(id uuid.UUID) error
+		CreateUser(ctx context.Context, user *entities.User) (*entities.User, error)
+		FindAllUsers(ctx context.Context, page int, limit int) ([]*entities.User, int64, error)
+		FindUserById(ctx context.Context, id uuid.UUID) (*entities.User, error)
+		FindUserByHandler(ctx context.Context, handler string) (*entities.User, error)
+		FindUserByEmail(ctx context.Context, email string) (*entities.User, error)
+		PatchUser(ctx context.Context, id uuid.UUID, user *entities.User) error
+		DeleteUser(ctx context.Context, id uuid.UUID) error
 	}
 
 	SessionUseCase interface {
-		CreateSession() (*entities.Session, error)
-		FindSessionByID(id uuid.UUID) (*entities.Session, error)
-		RevokeSession(email string) error
-		DeleteSession(id uuid.UUID) error
+		CreateSession(ctx context.Context) (*entities.Session, error)
+		FindSessionByID(ctx context.Context, id uuid.UUID) (*entities.Session, error)
+		RevokeSession(ctx context.Context, email string) error
+		DeleteSession(ctx context.Context, id uuid.UUID) error
 	}
 )
--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"context"
+
 	"github.com/google/uuid"
 	"github.com/mxilia/go-user-microservice.git/internal/entities"
 	"github.com/mxilia/go-user-microservice.git/internal/repo"
@@ -18,41 +20,56 @@ func NewUserService(userRepository repo.UserRepository) usecase.UserUseCase {
 	}
 }
 
-func (u *UserService) CreateUser(user *entities.User) (*entities.User, error) {
+func (u *UserService) CreateUser(ctx context.Context, user *entities.User) (*entities.User, error) {
 	if user == nil {
 		return nil, appError.ErrInvalidInput
 	}
-	return u.userRepository.SaveUser(user)
+	return u.userRepository.SaveUser(ctx, user)
 }
 
-func (u *UserService) FindAllUsers(page int, limit int) ([]*entities.User, int64, error) {
+func (u *UserService) FindAllUsers(ctx context.Context, page int, limit int) ([]*entities.User, int64, error) {
 	if page < 1 || limit < 1 {
 		return nil, 0, appError.ErrInvalidInput
 	}
 
-	users, totalCount, err := u.userRepository.FindAllUsers(page, limit)
+	users, totalCount, err := u.userRepository.FindAllUsers(ctx, page, limit)
 	if err != nil {
 		return nil, 0, err
 	}
 	return users, totalCount, err
 }
 
-func (u *UserService) FindUserByEmail(email string) (*entities.User, error) {
-	panic("unimplemented")
+func (u *UserService) FindUserByEmail(ctx context.Context, email string) (*entities.User, error) {
+	if email == "" {
+		return nil, appError.ErrInvalidInput
+	}
+	return u.userRepository.FindUserByEmail(ctx, email)
 }
 
-func (u *UserService) FindUserByHandler(handler string) (*entities.User, error) {
-	panic("unimplemented")
+func (u *UserService) FindUserByHandler(ctx context.Context, handler string) (*entities.User, error) {
+	if handler == "" {
+		return nil, appError.ErrInvalidInput
+	}
+	return u.userRepository.FindUserByHandler(ctx, handler)
 }
 
-func (u *UserService) FindUserById(id uuid.UUID) (*entities.User, error) {
-	panic("unimplemented")
+func (u *UserService) FindUserById(ctx context.Context, id uuid.UUID) (*entities.User, error) {
+	if id == uuid.Nil {
+		return nil, appError.ErrInvalidInput
+	}
+	return u.userRepository.FindUserById(ctx, id)
 }
 
-func (u *UserService) PatchUser(id uuid.UUID, user *entities.User) error {
-	panic("unimplemented")
+func (u *UserService) PatchUser(ctx context.Context, id uuid.UUID, user *entities.User) error {
+	if id == uuid.Nil || user == nil {
+		return appError.ErrInvalidInput
+	}
+	return u.userRepository.PatchUser(ctx, id, user)
 }
 
-func (u *UserService) DeleteUser(id uuid.UUID) error {
-	panic("unimplemented")
+func (u *UserService) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	if id == uuid.Nil {
+		return appError.ErrInvalidInput
+	}
+	return u.userRepository.DeleteUser(ctx, id)
 }
